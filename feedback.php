ac64886fa87ce67c0e04a7921cfc4ab25366905d
plus-team <81315362+plus-team@users.noreply.github.com>
2021-03-25 19:15:43 +0100

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,9 +1,9 @@
 <?php
-// 1 : on ouvre le fichier
+
 $monfichier = fopen('données.txt', 'r+');
 
-// 2 : on fera ici nos opérations sur le fichier...
 
-// 3 : quand on a fini de l'utiliser, on ferme le fichier
-fclose($monfichier);
+
+
+fclose($feedback.php);
 ?>
